43a29e40f217c6ad5f84a005be7050e4dc8e9a5c
Popov Philipp <philolippo@gmail.com>
2024-01-16 15:53:34 +0100

    Add assets crate

---
+++ src/assets.rs
@@ -0,0 +1,4 @@
+pub const BACKGROUND_IMAGE: &[u8] = include_bytes!("../resources/background_tower_1.png");
+pub const QUAS: &[u8] = include_bytes!("../resources/quas.png");
+pub const WEX: &[u8] = include_bytes!("../resources/wex.png");
+pub const EXORT: &[u8] = include_bytes!("../resources/exort.png");
